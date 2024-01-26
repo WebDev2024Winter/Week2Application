1be8900eb6b94af386f79d100a4a6b3a476ba467
clamcatfish <clam0540@gmail.com>
2024-01-25 22:18:57 -0700

    added button functionality with calculations

--- jscript.js
+++ jscript.js
@@ -1,5 +1,27 @@
-const Calculate = (type1, type2, val1, val2) => {
-
+const Calculate = (type1, type2, value1) => {
+    if (type1 === type2) {
+        return value1;
+    }
+    
+    switch (`${type1}-${type2}`) {
+        case type1 === type2:
+          return value1;
+        case 'Pounds-Kilograms':
+          return value1 * 0.453592;
+        case 'Kilograms-Pounds':
+          return value1 * 2.20462;
+        case 'Kilometers-Miles':
+          return value1 * 0.621371; 
+        case 'Miles-Kilometers':
+          return value1 * 1.60934;
+        case 'Celsius-Fahrenheit':
+          return (value1 * 9/5) + 32;
+        case 'Fahrenheit-Celsius':
+          return (value1 - 32) * 5/9;
+        default:
+          console.error('Invalid conversion type');
+          return null;
+      }
 }
 
 
@@ -23,5 +45,14 @@ const updateField = () => {
         document.getElementById("message").innerText = " ";
     }
 
+    
+
+}
+
+const updateToAnswerBox = () => {
+    const type1 = document.getElementById("Unit1").innerText
+    const type2 = document.getElementById("Unit2").innerText
+    const value1 = document.getElementById("fromAnswerBox").value
+    document.getElementById("toAnswerBox").value = Calculate(type1, type2, value1)
 
 }
