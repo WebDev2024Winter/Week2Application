b3a07b9361411eeb92851201d1a376018ced9b27
thomasutty004 <130857875+thomasutty004@users.noreply.github.com>
2024-02-06 21:08:44 -0700

    Update jscript.js
    documented js file

--- jscript.js
+++ jscript.js
@@ -1,13 +1,22 @@
+/**
+ * Calculate function that converts values between different types of units
+ * @param {string} type1 - The type of the value to convert from
+ * @param {string} type2 - The type of the value to convert to
+ * @param {string} value1 - The value to convert
+ * @returns {number} - The converted value or null if input is invalid
+ */
 const Calculate = (type1, type2, value1) => {
   const number = parseFloat(value1);
+
+  if (isNaN(number)) {
+    return null;
+  }
 
   if (type1 === type2) {
     return number;
   }
 
   switch (`${type1}-${type2}`) {
-    case type1 === type2:
-      return number;
     case "Pounds-Kilograms":
       return number * 0.453592;
     case "Kilograms-Pounds":
@@ -26,6 +35,9 @@ const Calculate = (type1, type2, value1) => {
   }
 };
 
+/**
+ * UpdateField function that updates the selected units on the page
+ */
 const updateField = () => {
   let selection1 = document.getElementById("selection1");
   let firstType = selection1.options[selection1.selectedIndex].text;
@@ -43,21 +55,33 @@ const updateField = () => {
     document.getElementById("message").innerText =
       "Sorry, you cannot convert " + firstType + " to " + secondType + ".";
   } else {
-    document.getElementById("message").innerText = " ";
+    document.getElementById("message").innerText = "";
   }
 };
 
+/**
+ * UpdateAnswerBox function that computes the converted values and updates the output box
+ * @param {string} fromType - The ID of the element containing the "from" unit
+ * @param {string} toType - The ID of the element containing the "to" unit
+ * @param {string} inputBox - The ID of the input box containing the value to convert
+ * @param {string} outputBox - The ID of the output box to display the converted value
+ */
 const updateAnswerBox = (fromType, toType, inputBox, outputBox) => {
   const type1 = document.getElementById(fromType).innerText;
   const type2 = document.getElementById(toType).innerText;
   let value1 = document.getElementById(inputBox).value;
 
   const splitValues = value1.split(" ");
-  const calcultedValues = splitValues.map((value) =>
+  const calculatedValues = splitValues.map((value) =>
     Calculate(type1, type2, value).toFixed(2)
   );
-  console.log("calculatedvalues:", calcultedValues);
-  value1 = calcultedValues.join(" ");
 
+  if (calculatedValues.includes(null)) {
+    document.getElementById("message").innerText = "Sorry, you entered a letter. Please enter a valid number.";
+    return;
+  }
+
+  document.getElementById("message").innerText = "";
+  value1 = calculatedValues.join(" ");
   document.getElementById(outputBox).value = value1;
 };
